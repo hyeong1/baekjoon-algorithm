b50548e3993a68c6ab15e759263d3884d288ba5e
MinHyeong Lee <86397095+hyeong1@users.noreply.github.com>
2024-03-25 17:40:24 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 556 ms, Memory: 38412 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,27 +1,29 @@
 import sys
-sys.setrecursionlimit(10**6)
-
-
-def dfs(graph, node, visited):
-    if visited[node]:
-        return
-    visited[node] = True
-    for i in range(1, N+1):
-        if not visited[i] and graph[node][i]:  # 아직 방문 하지 않았고 연결 되어 있으면 탐색
-            dfs(graph, i, visited)
-
+sys.setrecursionlimit(10**9)
 
 N, M = map(int, input().split())
 graph = [[False] * (N+1) for _ in range(N+1)]
-for _ in range(M):
-    a, b = map(int, sys.stdin.readline().split())
-    graph[a][b] = graph[b][a] = True
-
-count = 0
 visited = [False] * (N+1)
+for _ in range(M):
+    u, v = map(int, sys.stdin.readline().split())
+    graph[u][v] = graph[v][u] = True
+
+
+def dfs(node):
+    global visited
+
+    if visited[node]:
+        return
+    visited[node] = True
+    for j in range(1, N+1):
+        if not visited[j] and graph[node][j]:
+            dfs(j)
+
+
+result = 0
 for i in range(1, N+1):
     if not visited[i]:
-        dfs(graph, i, visited)
-        count += 1
+        dfs(i)
+        result += 1
 
-print(count)
+print(result)
