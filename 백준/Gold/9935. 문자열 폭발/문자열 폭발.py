be5b73f4a1420a2e40fb00bb04a7f3da68786a41
MinHyeong Lee <86397095+hyeong1@users.noreply.github.com>
2024-03-28 10:03:37 +0900

    [Gold IV] Title: 문자열 폭발, Time: 748 ms, Memory: 42300 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -5,7 +5,7 @@ stack = []
 for s in string:
     stack.append(s)
     if ''.join(stack[-len(bomb):]) == bomb:
-        for _ in range(len(bomb)):
+        for i in range(len(bomb)):
             stack.pop()
 
 result = ''.join(stack)
