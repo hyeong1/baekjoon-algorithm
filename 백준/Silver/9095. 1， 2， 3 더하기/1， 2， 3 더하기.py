4860e1575d931dceeb3701340e71d0c8311dd1e9
MinHyeong Lee <86397095+hyeong1@users.noreply.github.com>
2024-03-29 15:56:54 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,19 +1,11 @@
 T = int(input())
+dp = [1] * 11
+dp[2] = 2
+dp[3] = 4
+
+for i in range(4, 11):
+    dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
 
 for _ in range(T):
     N = int(input())
-    if N == 1 or N == 2:
-        print(N)
-        continue
-    if N == 3:
-        print(4)
-        continue
-
-    dp = [1] * (N+1)
-    dp[2] = 2
-    dp[3] = 4
-
-    for i in range(4, N+1):
-        dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
-
     print(dp[N])
