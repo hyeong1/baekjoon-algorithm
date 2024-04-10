27833cc9cf031567ac120459ca587bbb73ab3e28
MinHyeong Lee <86397095+hyeong1@users.noreply.github.com>
2024-04-10 10:05:59 +0900

    [Silver V] Title: 막대기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.py
+++ 백준/Silver/1094. 막대기/막대기.py
@@ -3,11 +3,8 @@ x = int(input())
 sticks = [64, 32, 16, 8, 4, 2, 1]
 count = 0
 
-for i in range(len(sticks)):
-    if x >= sticks[i]:
-        x -= sticks[i]
+for i in range(7):
+    if (x & (1 << i)) > 0:  # i번째 비트가 1인지 확인
         count += 1
-    if x == 0:
-        break
 
 print(count)
