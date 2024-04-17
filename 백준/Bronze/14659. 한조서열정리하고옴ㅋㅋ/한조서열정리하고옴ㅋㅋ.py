c96ce93fa185a16ad24127279c36700eb83e358a
MinHyeong Lee <86397095+hyeong1@users.noreply.github.com>
2024-04-17 10:54:28 +0900

    [Bronze I] Title: 한조서열정리하고옴ㅋㅋ, Time: 56 ms, Memory: 34432 KB -BaekjoonHub

--- 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.py
+++ 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.py
@@ -3,14 +3,17 @@ import sys
 N = int(sys.stdin.readline())
 arrows = list(map(int, sys.stdin.readline().split()))
 max_count = 0
+high = 0
 
 for i in range(N):
     count = 0
-    for j in range(i+1, N):
-        if arrows[i] > arrows[j]:
-            count += 1
-        else:
-            break
-    max_count = max(max_count, count)
+    if arrows[i] >= high:
+        high = arrows[i]
+        for j in range(i+1, N):
+            if arrows[i] > arrows[j]:
+                count += 1
+            else:
+                break
+        max_count = max(max_count, count)
 
 print(max_count)
