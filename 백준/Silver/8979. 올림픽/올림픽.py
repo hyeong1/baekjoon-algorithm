13b99160f5a659b613823c2d734b38631a0d878a
MinHyeong Lee <86397095+hyeong1@users.noreply.github.com>
2024-04-20 11:26:24 +0900

    [Silver V] Title: 올림픽, Time: 40 ms, Memory: 31120 KB, Score: 45 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -4,7 +4,11 @@ N, K = map(int, sys.stdin.readline().split())
 info = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
 info.sort(key=lambda x: (x[1], x[2], x[3]), reverse=True)
-for i in range(N-1):
+for i in range(N):
     if info[i][0] == K:
-        print(i)
-        break
+        if info[i][1:] == info[i-1][1:]:
+            print(i)
+            break
+        else:
+            print(i+1)
+            break
