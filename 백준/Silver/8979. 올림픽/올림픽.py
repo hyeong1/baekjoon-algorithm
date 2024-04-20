b633ac40dccc0a242d7c96f210ff4c26641e06b8
MinHyeong Lee <86397095+hyeong1@users.noreply.github.com>
2024-04-20 13:21:45 +0900

    [Silver V] Title: 올림픽, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,14 +1,19 @@
 import sys
 
 N, K = map(int, sys.stdin.readline().split())
-info = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-info.sort(key=lambda x: (x[1], x[2], x[3]), reverse=True)
+gold, silver, bronze = [0] * (N+1), [0] * (N+1), [0] * (N+1)
+medals = {}
 for i in range(N):
-    if info[i][0] == K:
-        if info[i][1:] == info[i-1][1:]:
-            print(i)
-            break
-        else:
-            print(i+1)
-            break
+    idx, g, s, b = map(int, sys.stdin.readline().split())
+    medals[idx] = (g, s, b)
+
+K_medals = medals[K]
+
+cnt = 1
+for n, n_medals in medals.items():
+    if n != K:
+        if n_medals > K_medals:
+            cnt += 1
+
+print(cnt)
