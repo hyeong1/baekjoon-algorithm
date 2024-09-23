81d01e619f5a8efeca3694f08b5437f9d7af2862
MinHyeong Lee <86397095+hyeong1@users.noreply.github.com>
2024-09-23 21:34:31 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,6 +1,4 @@
 select id, email, first_name, last_name
 from developer_infos
-where skill_1 like "py%"
-   or skill_2 like "py%"
-   or skill_3 like "py%"
+where "Python" in (skill_1, skill_2, skill_3)
 order by id
