658f62650649b1327b1a80d585dffd20ed020ae6
MinHyeong Lee <86397095+hyeong1@users.noreply.github.com>
2025-03-11 21:09:59 +0900

    [Silver IV] Title: 제로, Time: 144 ms, Memory: 114904 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,12 +1,11 @@
-import sys
 k = int(input())
 stack = []
 
-for i in range(k):
-    num = int(sys.stdin.readline())
-    if num == 0:
+for _ in range(k):
+    n = int(input())
+    if n == 0 and len(stack) > 0:
         stack.pop()
     else:
-        stack.append(num)
+        stack.append(n)
 
 print(sum(stack))
